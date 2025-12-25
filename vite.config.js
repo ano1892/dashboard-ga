053ec9af545e8ff0fdd18f6c2c288e58a85ccca3
Agung <ano1892@gmail.com>
2025-12-25 13:21:34 +0700

    Fix chunk size warning

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1600, // Menambah batas ukuran file agar warning hilang
+    rollupOptions: {
+        output: {
+            manualChunks(id) {
+                if (id.includes('node_modules')) {
+                    return 'vendor';
+                }
+            }
+        }
+    }
+  },
 })
